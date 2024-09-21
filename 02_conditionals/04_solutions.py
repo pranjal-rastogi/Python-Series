b65530bcb6da5fb5763c6521296ec728248e2661
Pranjal Rastogi <126326239+pranjal-rastogi@users.noreply.github.com>
2024-09-21 17:03:54 +0000

    Fruit Ripeness Checker

---
+++ 02_conditionals/04_solutions.py
@@ -0,0 +1,12 @@
+fruit = "Banana"
+color = "Yellow"
+
+if fruit == "Banana":
+    if color == "Green":
+        print("unripe")
+    elif color == "Yellow":
+        print("Ripe")
+    elif color == "Brown":
+        print("OverRipe")
+else:
+    print("Not provide information")
